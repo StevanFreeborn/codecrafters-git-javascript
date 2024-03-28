7d74355d6c2317c31d568c4558849094501585d8
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-03-28 12:53:44 -0500

    fix: export

--- app/commands.js
+++ app/commands.js
@@ -1,7 +1,7 @@
 import { mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
-function createGitDirectory() {
+export function createGitDirectory() {
   mkdirSync(join(__dirname, '.git'), { recursive: true });
   mkdirSync(join(__dirname, '.git', 'objects'), { recursive: true });
   mkdirSync(join(__dirname, '.git', 'refs'), { recursive: true });
@@ -9,7 +9,3 @@ function createGitDirectory() {
   writeFileSync(join(__dirname, '.git', 'HEAD'), 'ref: refs/heads/main\n');
   console.log('Initialized git directory');
 }
-
-export default {
-  createGitDirectory,
-};
